f35f78dca78885a74b1cfd13c3a98c4fdd19f9fb
Lazaro <cuervo.piedritas@gmail.com>
2017-10-22 21:33:28 -0300

    basic start application and connect to mongo DB

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/sports');
+var dbConection = mongoose.connection;
+
+app.get("/", function(req, res){
+    res.send("Home Page");
+})
+
+app.listen(2000);
+console.log('Running on port 2000...');

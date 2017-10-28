2e58cff2eae2b68c4135bb02992e761bbd90594c
Lazaro <cuervo.piedritas@gmail.com>
2017-10-28 13:42:10 -0300

    added match model with mongoose schema

---
+++ models/matchModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const maxTeamsPerMatch = 2;
+
+//Define the collection schema
+var matchSchema = mongoose.Schema({
+    id:{
+        type: String,
+        auto: true
+    },
+    startDate:{
+        type: Date,
+        required: true
+    },
+    status:{
+        type: String,
+        default: "Not Started"
+    },
+    teams:[
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "teams",
+            validate: [arrayLimit]
+        }
+    ]
+})
+
+//Validate the amount of teams assigned to the match
+function arrayLimit(val) {
+    return val.length <= maxTeamsPerMatch;
+  }
+
+//Export the teams collection
+module.exports = mongoose.model('matches', matchSchema);

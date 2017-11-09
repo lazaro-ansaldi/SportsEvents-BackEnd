5caa71439fe8fad6030a4fba4beef10d89f578d8
Lazaro <cuervo.piedritas@gmail.com>
2017-11-09 00:45:06 -0300

    Add basic event schema

---
+++ models/eventModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+//Define the collection schema
+var eventSchema = mongoose.Schema({
+    createDate:{
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    eventType:{
+        type: String,
+        required: true,
+        enum: ["Start", "Finish", "Goal"]
+    },
+    match:{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "matches"
+    }
+});
+
+module.exports = mongoose.model("events", eventSchema);
